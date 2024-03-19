50a33aab5e19e92dc66eb2e9d393c94a2dbcc842
miervaldis42 <31883227+miervaldis42@users.noreply.github.com>
2024-03-19 10:02:20 +0400

    test & feat (client) : 'Employee List' Page Creation
    - Create a test suite for the future 'Employee List' page
    - Create the page component for 'Employee List'

---
+++ src/app/authenticated/employees/page.tsx
@@ -0,0 +1,11 @@
+/**
+ * 'Employee List' page
+ * @description Uses as the Home page of the application
+ *
+ * @returns {JSX.Element} Page
+ */
+function Page(): JSX.Element {
+  return <h2>Employee List</h2>;
+}
+
+export default Page;
