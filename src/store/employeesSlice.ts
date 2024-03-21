9471c1ac7178bf8009856024f35d3b399fd6c87c
miervaldis42 <31883227+miervaldis42@users.noreply.github.com>
2024-03-21 13:18:37 +0400

    feat (client) : Redux Toolkit Store Creation
    - Create the store code in a new dedicated folder '/store'
    - Implement the backbone of a slice to be used with the employees data

---
+++ src/store/employeesSlice.ts
@@ -0,0 +1,43 @@
+// Imports
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+// Types
+import { EmployeesType, EmployeeType } from "@customTypes/employeeType";
+
+// Initial State
+const initialState: EmployeesType = [
+  {
+    firstname: "Sherlock",
+    lastname: "Holmes",
+    birthdate: new Date(1854, 0, 6),
+    startDate: new Date(2024, 0, 1),
+    address: {
+      street: "221B Baker Street",
+      city: "London",
+      state: {
+        name: "Alabama",
+        abbreviation: "AL",
+      },
+      code: "NW16XE",
+    },
+    department: "Engineering",
+  },
+];
+
+/**
+ * Employee Slice
+ *
+ */
+const employeesListSlice = createSlice({
+  name: "employeesListSlice",
+  initialState,
+  reducers: {
+    resetState: (state) => Object.assign(state, initialState),
+    add: (state, action: PayloadAction<EmployeeType>) => {
+      state.push(action.payload);
+    },
+  },
+});
+
+export const { add } = employeesListSlice.actions;
+export const employeesReducer = employeesListSlice.reducer;
