0424d47e59e274049515858a6f56f3fc24702ca9
miervaldis42 <31883227+miervaldis42@users.noreply.github.com>
2024-03-31 18:43:24 +0400

    feat (client) : Input Component Creation
    - Create a component to create custom inputs
    - List all necessary types related to the component
    - Use the previous created `CustomIcon` component to display an icon next to the input if necessary
    - Add a list of props to fully customize the input with label, button or specific type of input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,71 @@
+// Components
+import CustomIcon from "../Icon";
+
+// Types
+import { ChangeEventHandler, MouseEventHandler } from "react";
+import { IconNameTypes } from "@assets/iconLibrary";
+type CustomInputPropsType = {
+  idName: string;
+  hasLabel?: boolean;
+  labelContent?: string;
+  type: "text";
+  placeholder?: string;
+  value: string;
+  onChangeHandler?: ChangeEventHandler<HTMLInputElement>;
+  onClickHandler?: MouseEventHandler<HTMLButtonElement>;
+  stylingInputGroup?: string;
+  stylingInput?: string;
+  hasButton: boolean;
+  stylingButton?: string;
+  iconName?: IconNameTypes;
+  stylingIcon?: string;
+};
+
+/**
+ * @name CustomInput
+ * @component
+ * @description Custom Input component.
+ *
+ * @param {CustomInputPropsType} props Elements to customize the input which will be displayed
+ * @returns
+ */
+function CustomInput({
+  idName,
+  hasLabel = false,
+  labelContent = idName,
+  type,
+  placeholder,
+  value = "",
+  onChangeHandler,
+  onClickHandler,
+  stylingInputGroup,
+  stylingInput,
+  hasButton = false,
+  stylingButton,
+  iconName = "magnifier",
+  stylingIcon,
+}: CustomInputPropsType) {
+  return (
+    <div className={stylingInputGroup}>
+      <label htmlFor={idName} className={hasLabel ? "visible" : "hidden"}>
+        {labelContent}
+      </label>
+      <input
+        id={idName}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChangeHandler}
+        className={stylingInput}
+      />
+
+      {hasButton && (
+        <button onClick={onClickHandler} className={stylingButton}>
+          <CustomIcon name={iconName} styling={stylingIcon} />
+        </button>
+      )}
+    </div>
+  );
+}
+
+export default CustomInput;
