0f79981b37c85605de3f5fee71a85a82dfbb84d6
miervaldis42 <31883227+miervaldis42@users.noreply.github.com>
2024-04-01 15:23:39 +0400

    feat & style (client) : Custom Button Creation
    Create & Implement a new button to be able to fully customize it

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,44 @@
+// Imports
+import { MouseEventHandler, ReactNode } from "react";
+
+// Types
+interface ButtonPropsType {
+  type: "submit" | "button";
+  clickHandler?: MouseEventHandler<HTMLButtonElement> | (() => void);
+  disabled?: boolean;
+  styling?: string;
+  children: ReactNode;
+}
+
+/**
+ * @name CustomButton
+ * @component
+ * @description Button to fully customize.
+ *
+ * @param {ButtonPropsType} props Parameters to customize the components
+ * @returns {JSX.Element}
+ */
+function CustomButton({
+  type = "button",
+  clickHandler,
+  disabled,
+  styling,
+  children,
+}: ButtonPropsType): JSX.Element {
+  return (
+    <button
+      type={type}
+      onClick={clickHandler}
+      disabled={disabled}
+      className={
+        styling
+          ? styling
+          : `w-fit text-white bg-primary rounded-lg drop-shadow-md py-2 px-4 mt-4 hover:font-bold hover:bg-blue-800 hover:translate-y-px`
+      }
+    >
+      {children}
+    </button>
+  );
+}
+
+export default CustomButton;
