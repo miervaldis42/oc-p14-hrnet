907b2766d55537604dbeca7f3f9941d2a6dcf181
miervaldis42 <31883227+miervaldis42@users.noreply.github.com>
2024-04-01 16:29:01 +0400

    feat (client) : New Employee Form Backbone
    - Create the backbone of the form which will be used by the user to create a new employee
    - Use the newly created `<Button>` component to have a unified style & cohesion in the styling
    -  Use the `<Input>` to have the same behavior & style in the overall project

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,130 @@
+"use client";
+
+// Imports
+import { ChangeEvent } from "react";
+
+// Redux
+import { useDispatch } from "react-redux";
+import { add } from "@store/employeesSlice";
+
+// Routing
+import { useRouter } from "next/navigation";
+import routes from "@router/routes";
+
+// Components
+import Input from "./Input";
+import Button from "../Button";
+
+// Types
+import { EmployeeType } from "@customTypes/employeeType";
+
+/**
+ * @name NewEmployeeForm
+ * @component
+ * @description Form to create a new employee which will be displayed in the table in 'Employee List' page.
+ *
+ * @returns {JSX.Element}
+ */
+function NewEmployeeForm(): JSX.Element {
+  // Routing
+  const router = useRouter();
+
+  // Store
+  const dispatch = useDispatch();
+  const addNewEmployee = (newEmployee: EmployeeType) => {
+    dispatch(add(newEmployee));
+  };
+
+  const submitHandler: any = (event: ChangeEvent) => {
+    event.preventDefault();
+
+    const newEmployee: EmployeeType = {
+      firstname: "Henri",
+      lastname: "Thierry",
+      birthdate: new Date(1999, 0, 1),
+      startDate: new Date(2024, 0, 2),
+      address: {
+        street: "Stade de France",
+        city: "Paris",
+        state: {
+          name: "California",
+          abbreviation: "CA",
+        },
+        code: "75001",
+      },
+      department: "Human Resources",
+    };
+    addNewEmployee(newEmployee);
+
+    const employeeListPagePath = routes.employeeList.path;
+    const pathToEmployeeListPage =
+      typeof employeeListPagePath === "function"
+        ? employeeListPagePath()
+        : employeeListPagePath;
+    router.push(pathToEmployeeListPage);
+  };
+
+  return (
+    <form onSubmit={submitHandler} className="flex flex-col items-center">
+      {/* Employee Full Name */}
+      <Input
+        idName={"firstnameInput"}
+        hasLabel={true}
+        labelContent={"First Name"}
+        type={"text"}
+        placeholder={"Sherlock"}
+        autoCompleteText={"give-name"}
+        stylingInput="w-60 border-solid border border-slate-600 placeholder:italic placeholder:p-1 ml-2 mb-4"
+      />
+
+      <Input
+        idName={"lastnameInput"}
+        hasLabel={true}
+        labelContent={"Last Name"}
+        type={"text"}
+        placeholder={"Holmes"}
+        autoCompleteText={"family-name"}
+        stylingInput="w-60 border-solid border border-slate-600 placeholder:italic placeholder:p-1 ml-2 mb-4"
+      />
+
+      {/* Address */}
+      <fieldset className="border-solid border-2 border-primary py-2 px-4">
+        <legend className="px-2">Address</legend>
+
+        <Input
+          idName={"streetInput"}
+          hasLabel={true}
+          labelContent={"Street"}
+          type={"text"}
+          placeholder={"221B Baker Street"}
+          autoCompleteText={"street-address"}
+          stylingInput="w-60 border-solid border border-slate-600 placeholder:italic placeholder:p-1 ml-2 mb-4"
+        />
+
+        <Input
+          hasLabel={true}
+          labelContent={"City"}
+          idName={"cityInput"}
+          type={"text"}
+          placeholder={"London"}
+          autoCompleteText={"address-level2"}
+          stylingInput="w-60 border-solid border border-slate-600 placeholder:italic placeholder:p-1 ml-2 mb-4"
+        />
+
+        <Input
+          hasLabel={true}
+          labelContent={"Zip Code"}
+          idName={"zipcodeInput"}
+          type={"text"}
+          placeholder={"NW1 6XE"}
+          autoCompleteText={"postal-code"}
+          stylingInput="w-60 border-solid border border-slate-600 placeholder:italic placeholder:p-1 ml-2 mb-4"
+        />
+      </fieldset>
+
+      <Button type={"submit"}>Create Employee</Button>
+    </form>
+  );
+}
+
+export default NewEmployeeForm;
